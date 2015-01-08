56d88fe8c4812e12d3f94934ba6fec2fb82b1efb
jklmaynard <student@epicodus.com>
2015-01-08 11:01:43 -0800

    Added Second Spec

--- scrabble_spec.rb
+++ scrabble_spec.rb
@@ -4,5 +4,9 @@ require(scrabble.rb)
 describe('String#scrabble') do
   it('Returns '1' for values 'A,E,I,O,U,L,N,R,S,T'') do
     expect(("A").scrabble()).to(eq("1"))
+describe('String#scrabble') do
+  it('Returns '2' for values 'D,G'') do
+    expect(("D").scrabble()).to(eq("2"))
+
 end
 end
